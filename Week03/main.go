db1fc0c41e02bed04e742d75d004c75b5e7cea46
tianjiawei <810758235@qq.com>
2020-12-12 23:37:14 +0800

    第三周作业

--- Week03/main.go
+++ Week03/main.go
@@ -12,33 +12,38 @@ import (
 
 func main() {
 	errgroup, ctx := errgroup.WithContext(context.Background())
-	stop := make(chan struct{})
 	sign := make(chan os.Signal)
 
 	signal.Notify(sign, syscall.SIGHUP, syscall.SIGTERM, syscall.SIGINT)
 	server := &http.Server{Addr: ":8080"}
 
 	errgroup.Go(func() error {
-		http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-			w.Write([]byte("hello world!"))
-		})
-		return server.ListenAndServe()
+		return startServer(server)
 	})
 
 	errgroup.Go(func() error {
-		select {
-		case <-stop:
-			return server.Shutdown(ctx)
-		}
-	})
-
-	errgroup.Go(func() error {
-		select {
-		case <-sign:
-		}
-		server.Shutdown(ctx)
-		fmt.Println("http server drop out success！")
-		return nil
+		return shutDownServer(ctx, sign, server)
 	})
 	errgroup.Wait()
+}
+
+func startServer(server *http.Server) error {
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello world!"))
+	})
+	return server.ListenAndServe()
+}
+
+func shutDownServer(ctx context.Context, sign chan os.Signal, server *http.Server) error {
+	select {
+	case <-sign:
+	}
+	e := server.Shutdown(ctx)
+	if e == nil {
+		fmt.Println("http server drop out success！")
+	} else {
+		fmt.Println("http server drop out failed！")
+	}
+	//todo 释放资源
+	return nil
 }
