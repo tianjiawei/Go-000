f0aae6e0082204cfce9250d7aaa6aef4332eeda0
tianjiawei <810758235@qq.com>
2020-12-18 19:17:52 +0800

    week04

--- Week04/cmd/wire.go
+++ Week04/cmd/wire.go
@@ -3,18 +3,13 @@
 package main
 
 import (
+	"Go-000/Week04/configs"
 	"Go-000/Week04/internal/dao"
 	"Go-000/Week04/internal/db/mysql"
 	"Go-000/Week04/internal/service"
 	"github.com/google/wire"
 )
 
-func InitUserSrv() service.UserSrv {
-	wire.Build(service.NewUserSrv, dao.NewUserDao)
-	return service.UserSrv{}
-}
-
-func InitUserDao() dao.UserDao {
-	wire.Build(dao.NewUserDao, mysql.NewUserDB)
-	return dao.UserDao{}
+func InitUserSrv() *service.UserSrv {
+	panic(wire.Build(service.NewUserSrv, dao.NewUserDao, mysql.NewUserDB, configs.NewConfig))
 }
