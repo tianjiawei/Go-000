cb862d7aebbf9138021ad28af367114a1c30f490
tianjiawei <810758235@qq.com>
2020-12-19 00:02:49 +0800

    week04

--- Week04/cmd/main.go
+++ Week04/cmd/main.go
@@ -1,11 +1,48 @@
 package main
 
-import "Go-000/Week04/internal/server"
+import (
+	"Go-000/Week04/internal/server"
+	"context"
+	"fmt"
+	"golang.org/x/sync/errgroup"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+)
 
 func main() {
+	errgroup, ctx := errgroup.WithContext(context.Background())
+	sign := make(chan os.Signal)
+
+	signal.Notify(sign, syscall.SIGHUP, syscall.SIGTERM, syscall.SIGINT)
+	server := &http.Server{Addr: ":8080"}
+
+	errgroup.Go(func() error {
+		return startServer()
+	})
+
+	errgroup.Go(func() error {
+		return shutDownServer(ctx, sign, server)
+	})
+	errgroup.Wait()
+}
+
+func startServer() error {
 	router := server.NewHttp()
-	err := router.Run(":8080")
-	if err != nil {
-		panic("http shutdown")
+	return router.Run(":8080")
+}
+
+func shutDownServer(ctx context.Context, sign chan os.Signal, server *http.Server) error {
+	select {
+	case <-sign:
 	}
+	e := server.Shutdown(ctx)
+	if e == nil {
+		fmt.Println("http server drop out success！")
+	} else {
+		fmt.Println("http server drop out failed！")
+	}
+	//todo 释放资源
+	return nil
 }
